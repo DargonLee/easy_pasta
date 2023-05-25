1bc24394638073770aa6aac341caa57c78e0f7cb
Harlans <2461414445@qq.com>
2023-05-25 18:53:55 +0800

    增加文件的显示

--- lib/widget/item_card.dart
+++ lib/widget/item_card.dart
@@ -5,6 +5,7 @@ import 'package:easy_pasta/model/pasteboard_type.dart';
 import 'package:easy_pasta/widget/item_html_card.dart';
 import 'package:easy_pasta/widget/item_tiff_card.dart';
 import 'package:easy_pasta/widget/item_rtf_card.dart';
+import 'package:easy_pasta/widget/item_file_card.dart';
 
 class ItemCard extends StatelessWidget {
   final NSPboardTypeModel model;
@@ -23,11 +24,11 @@ class ItemCard extends StatelessWidget {
     }else if (model.ptype == NSPboardType.rtfType.name) {
       itemCard = ItemRtfCard(model: model, isSelected: isSelected,);
     }else if (model.ptype == NSPboardType.textRtfType.name) {
-      itemCard = ItemUtf8Card(model: model);
+      itemCard = ItemRtfCard(model: model, isSelected: isSelected,);
     }else if (model.ptype == NSPboardType.tiffType.name) {
       itemCard = ItemTiffCard(model: model, isSelected: isSelected,);
     }else if (model.ptype == NSPboardType.fileUrlType.name) {
-      itemCard = ItemUtf8Card(model: model);
+      itemCard = ItemFileCard(model: model, isSelected: isSelected,);
     }else if (model.ptype == NSPboardType.pngType.name) {
       itemCard = ItemUtf8Card(model: model);
     }else if (model.ptype == NSPboardType.appleNotesTypeType.name) {
---
+++ lib/widget/item_file_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:easy_pasta/model/pasteboard_model.dart';
+
+class ItemFileCard extends StatelessWidget {
+  final NSPboardTypeModel model;
+  final bool isSelected;
+
+  ItemFileCard({required this.model, this.isSelected = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 16, 10, 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          color: Colors.blueAccent,
+          width: isSelected ? 5.0 : 0.1,
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            model.pvalue,
+            maxLines: 3,
+          ),
+          Expanded(
+            flex: 1,
+            child: Icon(
+              Icons.folder,
+              size: 100,
+              color: Colors.blue,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
