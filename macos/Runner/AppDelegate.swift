62d66545abf650e5aa3316fdc610ac046f872677
Harlans <2461414445@qq.com>
2023-06-30 18:14:33 +0800

    替换 AppIcon

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,7 +4,6 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
     
-    
     var statusBarItem: NSStatusItem!
     
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -17,8 +16,9 @@ class AppDelegate: FlutterAppDelegate {
         
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
         if let button = self.statusBarItem.button {
-             statusBarItem.button?.title = "⏳"
-             button.action = #selector(showMainWindow(_:))
+//            statusBarItem.button?.title = "⏳"
+            configureMenuBarForDarkModeChange()
+            button.action = #selector(showMainWindow(_:))
         }
     }
     
@@ -32,10 +32,16 @@ class AppDelegate: FlutterAppDelegate {
         if isDarkMode() {
             imageName = "status_network_traffic_dark"
         }
-        self.statusBarItem.button?.image = Bundle.main.image(forResource: imageName)
+        let image = Bundle.main.image(forResource: imageName)
+        image?.size = NSSize(width: 22, height: 22)
+        self.statusBarItem.button?.image = image
     }
     
     func isDarkMode() -> Bool {
-        return UserDefaults.standard.value(forKey: "AppleInterfaceStyle") as! String == "Dark"
+        let name = NSApplication.shared.effectiveAppearance.name
+        if name == .darkAqua {
+            return true
+        }
+        return false
     }
 }
