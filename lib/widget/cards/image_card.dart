d233f5b0f092c5d052d353470206e934cb274a43
hailong.li <2461414445@qq.com>
2026-02-08 09:53:23 +0800

    Update image_card.dart

--- lib/widget/cards/image_card.dart
+++ lib/widget/cards/image_card.dart
@@ -11,24 +11,27 @@ class ImageContent extends StatelessWidget {
     super.key,
     required this.imageBytes,
     this.borderRadius = 8.0,
-    this.fit = BoxFit.cover,
+    this.fit = BoxFit.contain,
   });
 
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
-    
+    final resolvedFit = fit == BoxFit.fill ? BoxFit.contain : fit;
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(borderRadius),
       child: Container(
         decoration: BoxDecoration(
-          color: isDark 
-              ? AppColors.darkSecondaryBackground 
+          color: isDark
+              ? AppColors.darkSecondaryBackground
               : AppColors.lightSecondaryBackground,
         ),
         child: Image.memory(
           imageBytes,
-          fit: fit,
+          fit: resolvedFit,
+          alignment: Alignment.center,
+          gaplessPlayback: true,
           errorBuilder: (context, error, stackTrace) {
             return Center(
               child: Column(
@@ -36,8 +39,8 @@ class ImageContent extends StatelessWidget {
                 children: [
                   Icon(
                     Icons.broken_image_outlined,
-                    color: isDark 
-                        ? AppColors.darkTextTertiary 
+                    color: isDark
+                        ? AppColors.darkTextTertiary
                         : AppColors.lightTextTertiary,
                     size: 48,
                   ),
@@ -45,8 +48,8 @@ class ImageContent extends StatelessWidget {
                   Text(
                     '图片加载失败',
                     style: TextStyle(
-                      color: isDark 
-                          ? AppColors.darkTextSecondary 
+                      color: isDark
+                          ? AppColors.darkTextSecondary
                           : AppColors.lightTextSecondary,
                       fontSize: AppFontSizes.caption,
                     ),
